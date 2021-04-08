3069669f707c60b9d81351961432f13dccb146bb
Daniel Roberts <droberts@dimagi.com>
2021-04-07 19:16:01 -0500

    Remove vestigial and ill advised use of sessionid cookie

--- hqmedia.upload_controller.js
+++ hqmedia.upload_controller.js
@@ -38,7 +38,6 @@ function BaseHQMediaUploadController (uploader_name, marker, options) {
 
     // Stuff for processing the upload
     self.uploadParams = options.uploadParams || {};
-    self.sessionid = options.sessionid || null;
     self.licensingParams = options.licensingParams || [];
     self.uploadURL = options.uploadURL;
     self.processingURL = options.processingURL;
@@ -285,14 +284,6 @@ function BaseHQMediaUploadController (uploader_name, marker, options) {
                 postParams[key] = true;
             }
         }
-        var _cookie = document.cookie;
-        if (!/sessionid=/.exec(_cookie) && self.sessionid) {
-            if (_cookie) {
-                _cookie += '; ';
-            }
-            _cookie += 'sessionid=' + self.sessionid;
-        }
-        postParams['_cookie'] = _cookie;
         // With YUI 3.9 you can trigger downloads on a per file basis, but for now just keep the original behavior
         // of uploading the entire queue.
         self.uploader.uploadAll(self.uploadURL, postParams);
